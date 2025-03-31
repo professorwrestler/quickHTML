b2ab487b16989d554c5304b75467395d2524e622
professorwrestler <piercem047@gmail.com>
2025-03-31 03:29:57 -0400

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # quickHTML
+
 A bash script that automates HTML5 boilerplate along with Javascript and CSS files.
+
+Just run the script with one argument, which will be the name
+of the new directory. Once it's done, cd into said directory
+to see an index.html file, a js folder containing a main.js file,
+and a stylesheets folder containing a styles.css file. Each is
+linked in the automated html boilerplate, so everything you need
+to get started on a basic website is included.
---
+++ quickHTML.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# make sure we have a single argument, which will be the name of the new directory
+if [ -z "$1" ]; then
+  echo "Error: No arguments provided." >&2
+  echo "Provide a new directory name as the first argument."
+  exit 1
+fi
+
+# make new directory based on first argument
+mkdir "$1"
+# cd into new directory
+cd "$1"
+# make index.html file
+touch index.html
+# make javascript folder
+mkdir js
+# make main.js file inside javascript folder
+touch js/main.js
+# make CSS folder
+mkdir stylesheets
+# put styles.css into CSS folder
+touch stylesheets/style.css
+# put our HTML boilerplate inside the index.html file
+echo "<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>quickHTML</title>
+    <link rel="stylesheet" href="stylesheets/style.css">
+  </head>
+  <body>
+    <script src="js/main.js"></script>
+  </body>
+</html>" >> index.html
+
+cd ..
